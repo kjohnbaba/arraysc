f13f2a73a31351c09c2d5ced671dc7938bc4c677
kjohnbaba <105748315+kjohnbaba@users.noreply.github.com>
2022-06-19 02:40:53 -0400

    Create arrays.c

---
+++ arrays.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    int j;
+    for(i=0;i<5;i++){
+        for(j=0;j<5;j++){
+            printf("i is:%d, j is:%d \n",i,j);
+        }
+        
+    }
+    printf("End of program");
+
+    return 0;
+}
