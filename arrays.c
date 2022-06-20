fac315bc942b6cd6e12737bd3c3bb29c1fdafea1
kjohnbaba <105748315+kjohnbaba@users.noreply.github.com>
2022-06-20 10:44:26 -0400

    Update arrays.c

--- arrays.c
+++ arrays.c
@@ -4,13 +4,12 @@ int main()
 {
     int i;
     int j;
+    
     for(i=0;i<5;i++){
         for(j=0;j<5;j++){
             printf("i is:%d, j is:%d \n",i,j);
         }
-        
     }
     printf("End of program");
-
     return 0;
 }
